4366d359abf72e3659628feb6ee39abe32749de2
Krzysztof Bartkiewicz <bartkiewicz.krzysztof84@gmail.com>
2021-04-20 12:35:39 +0200

    Add Root component

--- src/root/Root.js
+++ src/root/Root.js
@@ -1,5 +1,7 @@
-import React, { Component } from "react";
-import axios from "axios";
+import React, { Component } from 'react';
+import axios from 'axios';
+import MovieList from '../components/MovieList/MovieList';
+import Router from '../routing/Router';
 
 class Root extends Component {
   state = {
@@ -15,15 +17,20 @@ class Root extends Component {
       .get(
         `https://api.themoviedb.org/3/movie/top_rated?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=1`
       )
-      .then((res) => console.log(res))
+      .then((res) => {
+        console.log(res);
+        this.setState({
+          movies: [...res.data.results],
+        });
+      })
       .catch((err) => console.error(err));
   };
 
   render() {
     return (
-      <div>
-        <h1>Movie app</h1>
-      </div>
+      <>
+        <Router movies={this.state.movies} />
+      </>
     );
   }
 }
